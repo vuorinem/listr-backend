085fc9bdac931123030e65643173a2a3d9a4cb91
Mikko Vuorinen <vuorinem@gmail.com>
2020-04-04 17:09:01 +0100

    Add socketIO connection and cancelling/reserving items

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express = require('express');
 import { MongoClient } from 'mongodb';
+import { listen } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT;
+const port = parseInt(process.env.PORT || "3003");;
 
-if(!process.env.MONGODB_CONNECTIONSTRING){
+const socketPort = parseInt(process.env.SOCKET_PORT || "3002");
+const io = listen(socketPort);
+
+if (!process.env.MONGODB_CONNECTIONSTRING) {
     console.error('MongoDB Connection String not set (MONGODB_CONNECTIONSTRING)');
     process.exit(1);
 }
@@ -22,19 +27,28 @@ client.connect((err, database) => {
 });
 
 const findList = (name: string) => {
-    console.log('Getting the DB');
     const db = dbContext.db('listr-db');
-    console.log(db);
+    const collection = db.collection('lists');
 
-    console.log('Getting the collection');
-    const collection = db.collection('lists')
-    console.log(collection);
-
-    console.log('Getting the list');
     return collection.findOne({
         'name': name,
     });
-}
+};
+
+const reserve = async (listName: string, itemLabel: string, count: number): Promise<boolean> => {
+    const db = dbContext.db('listr-db');
+    const collection = db.collection('lists');
+
+    const update = await collection.updateOne(
+        { name: listName },
+        { $inc: { 'items.$[item].reservedCount': count } },
+        { arrayFilters: [{ 'item.label': itemLabel }] },
+    );
+
+    return update.modifiedCount > 0;
+};
+
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log('Request started to ' + req.path);
@@ -42,16 +56,51 @@ app.use((req, res, next) => {
 });
 
 app.get('/list', async (req, res) => {
-    const name = req.query['name'];
-    console.log(`Finding list ${name}`);
+    const name = decodeURIComponent(req.query['name']);
     const list = await findList(name);
-    console.log('Found list:', list);
 
     if (list) {
         res.send(list);
     } else {
         res.status(404);
+        res.send('List not found');
+    }
+});
+
+app.post('/list/reserve', async (req, res) => {
+    const listName = decodeURIComponent(req.query['name']);
+    const itemLabel = decodeURIComponent(req.query['item']);
+    const isSuccess = await reserve(listName, itemLabel, 1);
+
+    if (isSuccess) {
+        io.emit(listName);
+        res.send();
+    } else {
+        res.status(400);
+        res.send('Error reserving item');
+    }
+});
+
+app.post('/list/cancel', async (req, res) => {
+    const listName = decodeURIComponent(req.query['name']);
+    const itemLabel = decodeURIComponent(req.query['item']);
+    const isSuccess = await reserve(listName, itemLabel, -1);
+
+    if (isSuccess) {
+        io.emit(listName);
+        res.send();
+    } else {
+        res.status(400);
+        res.send('Error reserving item');
     }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`);
+
+    socket.on('disconnect', function () {
+        console.log(`User ${socket.id} disconnected`);
+    });
+});
